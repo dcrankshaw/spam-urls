2d26d18c65cf8441b80f0b88594a85dc0233ef3e
Dan Crankshaw <dscrankshaw@gmail.com>
2015-02-11 00:46:47 -0800

    CW algorithm seems to be working

--- online_classification.py
+++ online_classification.py
@@ -22,21 +22,18 @@ xi_const = 1 + phi_const**2
 # algorithm from "Exact Convex Confidence-Weighted Learning"
 # http://nips.cc/Conferences/2008/Program/event.php?ID=1222
 
-def update_rule(x_i, true_y_i, mu_i, sigma_i, i):
+def update(x_i, true_y_i, mu_i, sigma_i, i):
     v_i = compute_v_i(x_i, sigma_i)
 
     # print v_i
     if v_i < 10**(-20):
+	print "uh oh"
 	return (mu_i, sigma_i)
     if math.isnan(v_i) or v_i == 0:
 	print "iter %d: bad v_i % f" % (i, v_i)
-	bad = True
     m_i = compute_m_i(x_i, true_y_i, mu_i)
     alpha_i = compute_alpha_i(v_i, xi_const, m_i, psi_const, phi_const)
     u_i = compute_u_i(alpha_i, v_i, phi_const)
-    if math.isnan(u_i) or u_i == 0:
-	print "iter %d: bad u_i: %f" % (i, u_i)
-	bad = True
     beta_i = compute_beta_i(alpha_i, phi_const, u_i, v_i)
 
     mu_prime = update_mu(mu_i, alpha_i, true_y_i, sigma_i, x_i)
@@ -44,22 +41,22 @@ def update_rule(x_i, true_y_i, mu_i, sigma_i, i):
     return (mu_prime, sigma_prime)
 
 def update_mu(mu_i, alpha_i, true_y_i, sigma_i, x_i):
-    mu_prime = mu_i + alpha_i * true_y_i * sigma_i.dot(x_i).transpose()
+
+    prod = la.sd_multiply(x_i, sigma_i)
+    mu_prime = la.sd_sum(la.s_scalar_mult(prod, alpha_i * true_y_i), mu_i)
+    # mu_prime = mu_i + alpha_i * true_y_i * la.sd_multiply(x_i, sigma_i)
     return mu_prime
 
-
 def make_prediction(x_i, mu_i):
-    t = np.sign(mu_i.dot(x_i).todense().item(0))
-    return t
-
+    return np.sign(la.sd_dot(x_i, mu_i))
 
 def compute_v_i(x_i, sigma_i):
-    v_i = x_i.transpose().dot(sigma_i).dot(x_i).todense().item(0)
+    prod = la.sd_multiply(x_i, sigma_i)
+    v_i = la.ss_dot(prod, x_i)
     return v_i
 
 def compute_m_i(x_i, true_y_i, mu_i):
-    m = (true_y_i*(mu_i.dot(x_i))).todense().item(0)
-    return m
+    return true_y_i * la.sd_dot(x_i, mu_i)
 
 def compute_alpha_i(v_i, xi_const, m_i, psi_const, phi_const):
     potential_alpha = 1.0 / (v_i*xi_const) * (-m_i*psi_const + np.sqrt(m_i**2 * phi_const**4 /4 + v_i*phi_const**2*xi_const))
@@ -71,64 +68,82 @@ def compute_u_i(alpha_i, v_i, phi_const):
 def compute_beta_i(alpha_i, phi_const, u_i, v_i):
     return alpha_i * phi_const / (np.sqrt(u_i) + v_i*alpha_i*phi_const)
 
-@profile
 def diag_sigma_update(sigma_i, alpha_i, phi_const, u_i, x_i):
-    x_nonzeros = sp.find(x_i)
-    x_squared = sp.coo_matrix((x_nonzeros[2]**2, (x_nonzeros[0], x_nonzeros[0])), shape=sigma_i.shape).todia()
-    sig_inv = diag_inverse(sigma_i)
-    new_sig_inv = sig_inv + alpha_i * phi_const*(1.0/np.sqrt(u_i))*x_squared
-    return diag_inverse(new_sig_inv)
+    x_squared = la.sparse_unary_op(x_i, lambda x: x**2)
+    sigma_inv = la.dense_unary_op(sigma_i, lambda x: 1.0 / x)
+    x_squared = la.s_scalar_mult(x_squared, alpha_i * phi_const / np.sqrt(u_i))
+    new_sigma_inv = la.sd_sum(x_squared, sigma_inv)
+    return la.dense_unary_op(new_sigma_inv, lambda x: 1.0 / x)
 
-def diag_inverse(m):
-    m.data = 1.0 / m.data
-    return m
-    # d = sp.find(m)
-    # new_vals = 1.0 / d[2]
-    # return sp.coo_matrix((new_vals, (d[0], d[1])), shape=m.shape).todia()
+    # x_nonzeros = sp.find(x_i)
+    # x_squared = sp.coo_matrix((x_nonzeros[2]**2, (x_nonzeros[0], x_nonzeros[0])), shape=sigma_i.shape).todia()
+    # sig_inv = diag_inverse(sigma_i)
+    # new_sig_inv = sig_inv + alpha_i * phi_const*(1.0/np.sqrt(u_i))*x_squared
+    # return diag_inverse(new_sig_inv)
+
+# def diag_inverse(m):
+#     m.data = 1.0 / m.data
+#     return m
+#     # d = sp.find(m)
+#     # new_vals = 1.0 / d[2]
+#     # return sp.coo_matrix((new_vals, (d[0], d[1])), shape=m.shape).todia()
 
 
 def load_days(start, end):
-    max_dim = 0
-    days = []
+    paths = []
     for day in range(start, end):
 	path = "%s/Day%d.svm" % (base_path, day)
-	x, y = datasets.load_svmlight_file(path)
-	max_dim = max(max_dim, x.shape[1])
-	days.append((x, y))
-    return (days, max_dim)
+	paths.append(path)
+    return la.read_svmlight(paths)
 
 
 def main():
-    # samples = 500
-    # all_x, all_y = datasets.make_classification(n_samples=samples, n_features=dims)
-    # all_x, all_y = load_days(0, 2)
-    # for i in range(len(all_y)):
-	# if all_y[i] == 0:
-	#     all_y[i] = -1
-
-    all_days, max_dim = load_days(0, 1)
-    current_mu = sp.csr_matrix((1, max_dim))
-    current_sigma = sp.eye(max_dim)
+    xs, ys, max_dim = load_days(0, 1)
+    mu = np.zeros(max_dim)
+    sigma = np.ones(max_dim)
+    num_false_pos = 0
+    num_false_neg = 0
     num_wrong = 0
     total = 0
-    for d in all_days:
-	for i in range(len(d[1])):
-	    # x = d[0][i].toarray()[0] # have to extract 1st row because it makes a 2d array
-	    x = d[0][i].transpose()
-	    y = d[1][i]
-	    pred = make_prediction(x, current_mu)
-	    total += 1
-	    if y != pred:
-		num_wrong += 1
-	    current_mu, current_sigma = update_rule(x, y, current_mu, current_sigma, i)
-	    if total % 10 == 0:
-		# print "Processed %d examples" % total
-		print "Total: %d, wrong: %d" % (total, num_wrong)
-		break
-	
-	# print current_mu
+    num_examples = len(ys)
+    mu, sigma = update(xs[0], ys[0], mu, sigma, 0)
+    for i in range(1, num_examples):
+	x = xs[i]
+	y = ys[i]
+	pred = make_prediction(x, mu)
+	diff = y - pred
+	total += 1
+	if diff != 0:
+	    num_wrong += 1
+	if diff == -2:
+		num_false_pos += 1
+	if diff == 2:
+		num_false_neg += 1
+
+	mu, sigma = update(x, y, mu, sigma, i)
+	if i % 500 == 0:
+	    print "Processed %d" % i
+
+
+    # for d in all_days:
+	# for i in range(len(d[1])):
+	#     # x = d[0][i].toarray()[0] # have to extract 1st row because it makes a 2d array
+	#     x = d[0][i].transpose()
+	#     y = d[1][i]
+	#     pred = make_prediction(x, mu)
+	#     total += 1
+	#     if y != pred:
+	# 	num_wrong += 1
+	#     mu, current_sigma = update_rule(x, y, mu, current_sigma, i)
+	#     if total % 10 == 0:
+	# 	# print "Processed %d examples" % total
+	# 	print "Total: %d, wrong: %d" % (total, num_wrong)
+	# 	break
+	#
+	# # print mu
 
     print "Num wrong: %d" % num_wrong
+    print "FP: %d, FN: %d, combined: %d" % (num_false_pos, num_false_neg, num_false_pos + num_false_neg)
     print "Total: %d" % total
 
 
--- sparse_linalg.py
+++ sparse_linalg.py
@@ -12,25 +12,47 @@ class SparseVector:
 
 # sparse-dense vector multiply
 # returns a new sparse vector
-def sparse_dense_multiply(sp, de):
+def sd_multiply(sp, de):
     projected_de = de[sp.nz]
     prod = sp.data*projected_de
     return SparseVector(sp.nz, prod)
 
+#### CAVEAT: Assumes same sparsity structure
+def ss_dot(v1, v2):
+
+    if not np.array_equal(v1.nz, v2.nz):
+	raise Exception("Non matching sparsity")
+
+    return np.sum(v1.data*v2.data)
+
+
+    # # indexes in common
+    # intersect_nz = np.intersect1d(v1.nz, v2.nz)
+    #
+    # #indexes in v1.data of intersecting values
+    # v1_data_ind = np.in1d(v1.nz, v2.nz).nonzero()
+    # v2_data_ind = np.in1d(v2.nz, v1.nz).nonzero()
+    # prod = v1.data[v1_data_ind] * v2.data[v2_data_ind]
+    # return SparseVector(intersect_nz, prod)
+
 # sparse dense vector dot product
 # returns a scalar
-def sparse_dense_dot(sp, de):
+def sd_dot(sp, de):
     projected_de = de[sp.nz]
     dot = np.sum(sp.data*projected_de)
     return dot
 
 # sparse-dense element-wise sum
 # returns a dense vector
-def sparse_dense_sum(sp, de):
+def sd_sum(sp, de):
     new_de = de.copy()
     for (i, v) in zip(sp.nz, sp.data):
 	new_de[i] += v
     return new_de
+
+# multiply scalar times sparse vector
+def s_scalar_mult(sp, c):
+    return SparseVector(sp.nz, c*sp.data)
 
 # applies f to every element in de
 def dense_unary_op(de, f):
@@ -44,6 +66,7 @@ def sparse_unary_op(sp, f):
 def read_svmlight(files):
     xs = []
     ys = []
+    max_dim = 0
     
     for f in files:
 	with open(f, 'r') as current:
@@ -52,14 +75,15 @@ def read_svmlight(files):
 		ys.append(float(splits[0]))
 		x_vals = splits[1:]
 		nnz = len(x_vals)
-		nz = np.zeros(nnz)
+		nz = np.zeros(nnz, dtype=np.int_)
 		data = np.zeros(nnz)
 		for i in range(nnz):
 		    sp = x_vals[i].split(":")
 		    nz[i] = int(sp[0])
 		    data[i] = float(sp[1])
-		xs.append(SparseVector(nz, data))
-    return xs, ys
+		xs.append(SparseVector(nz - 1, data))
+		max_dim = max(max_dim, max(nz))
+    return (xs, ys, max_dim)
 
 
 
