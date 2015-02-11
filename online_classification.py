e5e625db116cfe6819f2a0fb8be540a671531bdc
Dan Crankshaw <dscrankshaw@gmail.com>
2015-02-10 18:33:04 -0800

    Changed to handle sparse matrices. Still reallllllllllly slow

--- online_classification.py
+++ online_classification.py
@@ -6,6 +6,9 @@ from scipy.stats import norm
 # from test_spam_classification import read_multiple_days
 from sklearn import datasets
 from sklearn.linear_model import LogisticRegression as LR
+import scipy.sparse as sp
+import warnings
+warnings.filterwarnings("error")
 
 
 base_path = "/Users/crankshaw/code/amplab/model-serving/data/spam-urls/url_svmlight"
@@ -25,7 +28,6 @@ xi_const = 1 + phi_const**2
 
 def update_rule(x_i, true_y_i, mu_i, sigma_i, i):
     bad = False
-    pred_y_i = make_prediction(x_i, mu_i)
     v_i = compute_v_i(x_i, sigma_i)
 
     # print v_i
@@ -42,22 +44,29 @@ def update_rule(x_i, true_y_i, mu_i, sigma_i, i):
 	bad = True
     beta_i = compute_beta_i(alpha_i, phi_const, u_i, v_i)
 
-    mu_prime = mu_i + (alpha_i * true_y_i * sigma_i*x_i)
+    mu_prime = mu_i + alpha_i * true_y_i * sigma_i.dot(x_i).transpose()
     sigma_prime = diag_sigma_update(sigma_i, alpha_i, phi_const, u_i, x_i)
     return (mu_prime, sigma_prime)
 
 
 def make_prediction(x_i, mu_i):
-    return np.sign(mu_i.dot(x_i))
+    res = sp.find(mu_i.dot(x_i))[2]
+    if len(res) == 0:
+	return 0
+    else:
+	return np.sign(res[0])
 
 
 def compute_v_i(x_i, sigma_i):
-    return (x_i*sigma_i).dot(x_i)
-    
+    v_i = x_i.transpose().dot(sigma_i).dot(x_i)
+    return sp.find(v_i)[2][0]
 
 def compute_m_i(x_i, true_y_i, mu_i):
-    m = true_y_i*(mu_i.dot(x_i))
-    return m
+    m = sp.find(true_y_i*(mu_i.dot(x_i)))[2]
+    if len(m) == 0:
+	return 0.0
+    else:
+	return m[0]
 
 def compute_alpha_i(v_i, xi_const, m_i, psi_const, phi_const):
     potential_alpha = 1.0 / (v_i*xi_const) * (-m_i*psi_const + np.sqrt(m_i**2 * phi_const**4 /4 + v_i*phi_const**2*xi_const))
@@ -70,14 +79,21 @@ def compute_beta_i(alpha_i, phi_const, u_i, v_i):
     return alpha_i * phi_const / (np.sqrt(u_i) + v_i*alpha_i*phi_const)
 
 def diag_sigma_update(sigma_i, alpha_i, phi_const, u_i, x_i):
-    return diag_inverse(diag_inverse(sigma_i) + alpha_i * phi_const*(1.0/np.sqrt(u_i))*(x_i**2))
+    x_nonzeros = sp.find(x_i)
+    x_squared = sp.dia_matrix(
+	    sp.coo_matrix((x_nonzeros[2]**2, (x_nonzeros[0], x_nonzeros[0])), shape=sigma_i.shape))
+    sig_inv = diag_inverse(sigma_i)
+    return diag_inverse(sig_inv + alpha_i * phi_const*(1.0/np.sqrt(u_i))*x_squared)
 
 def diag_inverse(m):
     # d = np.diagonal(m).copy()
-    d = m.copy()
-    for i in range(len(d)):
-	d[i] = 1.0/d[i]	
-    return d
+    d = sp.find(m)
+    new_vals = [1.0 / j for j in d[2]]
+    return sp.dia_matrix(sp.coo_matrix((new_vals, (d[0], d[1])), shape=m.shape))
+    # d = m.copy()
+    # for i in range(len(d)):
+	# d[i] = 1.0/d[i]	
+    # return d
 
 
 def load_days(start, end):
@@ -100,13 +116,14 @@ def main():
 	#     all_y[i] = -1
 
     all_days, max_dim = load_days(0, 1)
-    current_mu = np.zeros(max_dim)
-    current_sigma = np.ones(max_dim)
+    current_mu = sp.csr_matrix((1, max_dim))
+    current_sigma = sp.eye(max_dim)
     num_wrong = 0
     total = 0
     for d in all_days:
 	for i in range(len(d[1])):
-	    x = d[0][i].toarray()[0] # have to extract 1st row because it makes a 2d array
+	    # x = d[0][i].toarray()[0] # have to extract 1st row because it makes a 2d array
+	    x = d[0][i].transpose()
 	    y = d[1][i]
 	    pred = make_prediction(x, current_mu)
 	    total += 1
@@ -116,6 +133,7 @@ def main():
 	    if total % 10 == 0:
 		# print "Processed %d examples" % total
 		print "Total: %d, wrong: %d" % (total, num_wrong)
+		# break
 	
 	# print current_mu
 
